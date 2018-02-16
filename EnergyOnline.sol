e4a652dfac7b673845b78e34fcb783cbb74ec5ca
StefanOnGit <36464424+StefanOnGit@users.noreply.github.com>
2018-02-16 05:39:28 +0100

    Hopefully finished the solidity part

--- EnergyOnline.sol
+++ EnergyOnline.sol
@@ -88,4 +88,120 @@ contract UserBackend {
         require(owner == msg.sender);
         Origin(origin).transfer(_target, _amount);
     }
+    
+    function payForEnergy(address _supplier, address _meter, uint256 _kWh, uint256 _price) public {
+        require(owner == msg.sender);
+        Supplier(_supplier).requestEnergy(_meter, _kWh, _price);
+        sendTokens(_supplier, _price);
+        Supplier(_supplier).notifyEnergyPurchase();
+    }
+}
+
+contract Policy {
+    function grantRequest(address _trustedMeter, uint256 _tokenAmount, uint256 _kWh) public view;
+    function getPayout(address _trustedMeter, uint256 _producedKWh) public view returns(uint256);
+}
+
+contract Supplier {
+    address owner;
+    bool isOriginSet;
+    address origin;
+    address policy;
+    bool requestGranted;
+    GrantedRequest request;
+    mapping (address => bool) trustMeter;
+    
+    event ReleaseEnergy(address _meter, uint256 _kWh);
+    event Payout(address _meter, address _payoutAddress, uint256 _kWh, uint256 _payout);
+    
+    struct GrantedRequest {
+        address sender;
+        address meter;
+        uint256 kWh;
+        uint256 forPrice;
+        uint256 currentBalance;
+    }
+    
+    function Supplier() public {
+        owner = msg.sender;
+    }
+    
+    function getOwner() public view returns(address) {
+        return owner;
+    }
+    
+    function setOrigin(address _origin) public {
+        require(isOriginSet == false);
+        require(msg.sender == owner);
+        isOriginSet = true;
+        origin = _origin;
+    }
+    
+    function setPolicy(address _policy) public {
+        require(msg.sender == owner || msg.sender == address(this));
+        policy = _policy;
+    }
+    
+    function requestEnergy(address _meter, uint256 _kWh, uint256 _forPrice) public {
+        require(trustMeter[_meter] == true);
+        Policy(policy).grantRequest(_meter, _kWh, _forPrice);
+        requestGranted = true;
+        request = GrantedRequest(msg.sender, _meter, _kWh, _forPrice, myBalance());
+    }
+    
+    function notifyEnergyPurchase() public {
+        require(requestGranted);
+        require(request.sender == msg.sender);
+        require(request.currentBalance + request.forPrice == myBalance());
+        
+        requestGranted = false;
+        
+        // Releases the energy as event, meter has to notify and release 
+        // the energy physically if needed from the grid
+        ReleaseEnergy(request.meter, request.kWh);
+    }
+    
+    function notifyEnergyProduction(address _payoutAddress, uint256 _producedKWh) public {
+        require(trustMeter[msg.sender] == true);
+        uint256 payout = Policy(policy).getPayout(msg.sender, _producedKWh);
+        Origin(origin).transfer(_payoutAddress, payout);
+        Payout(msg.sender, _payoutAddress, _producedKWh, payout);
+    }
+    
+    function addMeter(address _meter) public {
+        require(msg.sender == owner);
+        trustMeter[_meter] = true;
+    }
+    
+    function removeMeter(address _meter) public {
+        require(msg.sender == owner);
+        trustMeter[_meter] = false;
+    }
+    
+    function myBalance() public view returns(uint256) {
+        return Origin(origin).balance_of(address(this));
+    }
+} 
+
+contract EWZ is Supplier, Policy {
+    uint256 kWhBuyPrice = 1e15;
+    uint256 kWhSellPrice = 2e15;
+    
+    function EWZ() Supplier() public {
+        setPolicy(this);
+    }
+    
+    function setPrices(uint256 _kWhSellPrice, uint256 _kWhBuyPrice) public {
+        require(msg.sender == getOwner());
+        kWhSellPrice = _kWhSellPrice;
+        kWhBuyPrice = _kWhBuyPrice;
+    }
+    
+    function grantRequest(address /*_trustedMeter*/, uint256 _tokenAmount, uint256 _kWh) public view {
+        require(_kWh >= _tokenAmount * kWhSellPrice);
+    }
+    
+    function getPayout(address /*_trustedMeter*/, uint256 _producedKWh) public view returns(uint256) {
+        return _producedKWh * kWhBuyPrice;
+    }
 }
